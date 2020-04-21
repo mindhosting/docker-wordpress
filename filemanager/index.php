a035fd750bd01263dfba42fd30d6803f48b69e55
Sakly Ayoub <saklyayoub@live.com>
2020-04-21 14:43:59 +0100

    no need to go out of public_html

--- filemanager/index.php
+++ filemanager/index.php
@@ -56,7 +56,7 @@ $default_timezone = 'Etc/UTC'; // UTC
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
 #$root_path = "/web_data";
-$root_path = '/web_data' ;
+$root_path = '/web_data/public_html' ;
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
